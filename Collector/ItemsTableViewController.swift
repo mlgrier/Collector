8c6f8dc1c6d142d30d4299284259bd4c21136e60
Unknown <mlgrier@gmail.com>
2017-08-29 21:21:29 -0500

    added the image to the table view

--- Collector/ItemsTableViewController.swift
+++ Collector/ItemsTableViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ItemsTableViewController: UITableViewController {
-
+    
     var items : [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
@@ -35,44 +35,49 @@ class ItemsTableViewController: UITableViewController {
         }
         
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return items.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         
         let item = items[indexPath.row]
-
+        
         cell.textLabel?.text = item.title
         
-
+        //added image to the table view
+        if let imageData = item.image {
+            
+            cell.imageView?.image = UIImage(data: imageData)
+        }
+        
         return cell
     }
- 
-
+    
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    
 }
